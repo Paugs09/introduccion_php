3d9eee8c23b19711c482360012d69f2231326ca8
paugs09 <mariagomez@colegioguanenta.edu.co>
2023-07-17 17:27:26 -0500

    Comentarios en php

---
+++ 1_comentarios.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentarios es PHP</title>
+</head>
+<body>
+    <h1>Comentarios en PHP</h1>
+    <!--Formas de incluir código php-->
+    <?php
+        //Comentario de una línea
+        /*Esto es un comentario de varias líneas*/
+    ?>
+
+    <!--
+    <%
+        codigo php
+    %>
+
+    <script language="php">
+
+    </script> -->
+
+</body>
+</html>
